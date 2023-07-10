2d26680aeb25f4b28b58c127002b570bb85c10f4
Daniel <danielnguy3n12@gmail.com>
2023-07-10 21:11:36 +1000

    click outside modal to and added alternate sign up modal

--- components/Choose-Plan/AuthModal.tsx
+++ components/Choose-Plan/AuthModal.tsx
@@ -2,33 +2,50 @@ import Image from "next/image";
 import { BsFillPersonFill } from "react-icons/bs";
 import { IoMdClose } from "react-icons/io";
 import Google from "../../public/images/google.png";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 
 interface Props {
   setLogin: Dispatch<SetStateAction<boolean>>;
 }
 
 function AuthModal({ setLogin }: Props) {
+  const [signIn, setSignIn] = useState(true);
+
   return (
-    <div className="modal__wrapper">
+    <div className="modal__wrapper" onClick={() => setLogin(false)}>
       <div className="modal">
-        <div className="modal__content">
-          <div className="modal__title">Login to Summarist</div>
-          <button className="btn btn__login--guest">
-            <figure className="btn__icon">
-              <BsFillPersonFill />
-            </figure>
-            <div className="btn__text">Login as a Guest</div>
-          </button>
-          <div className="modal__separator">
-            <div className="modal__separator--text">or</div>
-          </div>
-          <button className="btn btn__login--google">
-            <div className="btn__icon btn__icon--google">
-              <Image src={Google} alt="google" width={24} height={24} />
-            </div>
-            <div className="btn__text">Login with Google</div>
-          </button>
+        <div className="modal__content" onClick={e => e.stopPropagation()}>
+          {signIn ? (
+            <>
+              <div className="modal__title">Login to Summarist</div>
+              <button className="btn btn__login--guest">
+                <figure className="btn__icon">
+                  <BsFillPersonFill />
+                </figure>
+                <div className="btn__text">Login as a Guest</div>
+              </button>
+              <div className="modal__separator">
+                <div className="modal__separator--text">or</div>
+              </div>
+              <button className="btn btn__login--google">
+                <div className="btn__icon btn__icon--google">
+                  <Image src={Google} alt="google" width={24} height={24} />
+                </div>
+                <div className="btn__text">Login with Google</div>
+              </button>
+            </>
+          ) : (
+            <>
+              <div className="modal__title">Sign up Summarist</div>
+              <button className="btn btn__login--google">
+                <div className="btn__icon btn__icon--google">
+                  <Image src={Google} alt="google" width={24} height={24} />
+                </div>
+                <div className="btn__text">Sign Up With Google</div>
+              </button>
+            </>
+          )}
+
           <div className="modal__separator">
             <span>or</span>
           </div>
@@ -43,11 +60,20 @@ function AuthModal({ setLogin }: Props) {
               className="login__input"
               placeholder="Password"
             />
-            <button className="btn">Login</button>
+            {signIn ? (
+              <button className="btn">Login</button>
+            ) : (
+              <button className="btn">Sign Up</button>
+            )}
           </form>
         </div>
         <div className="modal__forgot-password">Forgot your password?</div>
-        <button className="modal__account">Don't have an account?</button>
+        
+        <button className="modal__account" onClick={() => setSignIn(!signIn)}>
+            {
+                signIn ? `Don't have an account?` : `Already have an account?`
+            }
+            </button>
         <div className="modal__close" onClick={() => setLogin(false)}>
           <IoMdClose />
         </div>
