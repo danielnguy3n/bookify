cdf6d26fe813f6488fcfdbee7d93ed4424206448
Daniel <danielnguy3n12@gmail.com>
2023-07-11 20:15:47 +1000

    styled empty library page

--- app/(user)/library/page.tsx
+++ app/(user)/library/page.tsx
@@ -1,7 +1,22 @@
 
 function Library() {
   return (
-    <div>page</div>
+    <div className="row">
+        <div className="container">
+            <div className="library__title">Saved Books</div>
+            <div className="library__subtitle">0 items</div>
+            <div className="library__empty-row">
+                <div className="empty__title">Save your favourite books!</div>
+                <div className="empty__description">When you save a book, it will appear here</div>
+            </div>
+            <div className="library__title">Finished</div>
+            <div className="library__subtitle">0 items</div>
+            <div className="library__empty-row">
+                <div className="empty__title">Done and dusted!</div>
+                <div className="empty__description">When you finish a book, you can find it here later.</div>
+            </div>
+        </div>
+    </div>
   )
 }
 
