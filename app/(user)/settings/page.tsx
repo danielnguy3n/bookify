a47ed58ac846e5d0f14ad3e875335e9560f0a067
Daniel <danielnguy3n12@gmail.com>
2023-07-11 20:25:39 +1000

    finish styling settings page

--- app/(user)/settings/page.tsx
+++ app/(user)/settings/page.tsx
@@ -1,8 +1,20 @@
-
 function Settings() {
   return (
-    <div>page</div>
-  )
+    <div className="row">
+      <div className="container">
+        <div className="settings__title">Settings</div>
+        <div className="settings__content">
+          <div className="settings__subtitle">Your Subscription Plan</div>
+          <div className="settings__desc">Basic</div>
+          <button className="btn settings__btn">Upgrade to Premium</button>
+        </div>
+        <div className="settings__content">
+          <div className="settings__subtitle">Email</div>
+          <div className="settings__desc">daniel@gmail.com</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Settings
+export default Settings;
