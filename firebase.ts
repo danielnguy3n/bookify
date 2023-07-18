3478f77621e33d944877c83d75eb43cd527cf560
Daniel <danielnguy3n12@gmail.com>
2023-07-18 19:42:42 +1000

    env local file

--- firebase.ts
+++ firebase.ts
@@ -8,13 +8,14 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCfeVMvpbk-B76_VEf5ciC-roOe3ovpOQY",
-  authDomain: "summarist-c16b5.firebaseapp.com",
-  projectId: "summarist-c16b5",
-  storageBucket: "summarist-c16b5.appspot.com",
-  messagingSenderId: "659973604812",
-  appId: "1:659973604812:web:00e34b5c54aaec21060969",
-  measurementId: "G-S1S3G0K03R",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+
 };
 
 // Initialize Firebase
